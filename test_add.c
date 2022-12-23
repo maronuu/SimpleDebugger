f22d86e930499673b3d04cc950abfbea87a4d5ba
maronuu <oguri.ytr.2000@gmail.com>
2022-12-24 02:17:28 +0900

    Update test case

--- test_add.c
+++ test_add.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
-void add(int *num, int val) {
-  *num += val;
+int add (int a, int b, int c) {
+    return a + b + c;
 }
 
 int main(int argc, char **argv, char **envp) {
-  int num = 0;
-  int tmp = 12;
-  printf("num = %d\n", num);
-  add(&num, 1);
-  printf("num = %d\n", num);
-  add(&num, 2);
-  printf("num = %d\n", num);
-  add(&num, 3);
-  printf("num = %d\n", num);
-  return 0;
+    int a, b, c;
+    a = 1;
+    b = 2;
+    c = 9;
+    printf("a = %d, b = %d, c = %d\n", a, b, c);
+
+    int d = add(a, b, 23); // 1(1) + 2(2) + 23(17) = 26(1a)
+    printf("%d + %d + %d = %d\n", a, b, 23, d);
+    int e = add(d, c, 54); // 26(1a) + 9(9) + 54(36) = 89(59)
+    printf("%d + %d + %d = %d\n", d, c, 54, e);
+    int f = add(e, 1, 7);  // 89(59) + 1(1) + 7(7) = 97(61)
+    printf("%d + %d + %d = %d\n", e, 1, 7, f);
+    return 0;
 }
